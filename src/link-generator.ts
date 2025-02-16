f67bc9a67915606f92d11ce99d2470a5aef0e003
luis-mfc <105208191+luis-mfc@users.noreply.github.com>
2025-02-16 12:02:52 +0000

    Refactor url path override logic (Fixes google_service_account) (#5)

--- src/link-generator.ts
+++ src/link-generator.ts
@@ -1,56 +1,83 @@
 import * as vscode from 'vscode';
 import * as providerData from '../assets/providers.json';
+import * as resourceData from '../assets/resources.json';
+import * as dataSourcesData from '../assets/data-sources.json';
 
 const configuration = vscode.workspace.getConfiguration('terraform-documentation-quick-links');
+const baseurl = configuration.get('baseurl') as string;
 
-const resourceTypes: Record<string, string> = {
-  "data": "data-sources",
-  "resource": "resources"
+// Mapping of overrides from extension config to handle non default providers and resources/data-sources that do not
+// follow the normal url path structure
+const overrides: Record<string, Record<string, string>> = {
+  provider: providerData,
+  resource: resourceData,
+  data: dataSourcesData,
 };
 
-function getCustomProviders() {
-  const customProvidersJSONString = configuration.get('providers') as string;
+// The same as the above but from the user settings (takes precedence to the above)
+const userOverrides = {
+  provider: getJsonConfig("providers"),
+  resource: getJsonConfig("resources"),
+  data: getJsonConfig("data-sources"),
+};
+
+// Mapping between tf keyword and matching url path
+const types: Record<string, string> = {
+  data: "data-sources",
+  resource: "resources"
+};
+
+// Utils
+
+function getJsonConfig(name: string) {
+  const JSONString = configuration.get(name) as string;
 
   try {
-    if (customProvidersJSONString) {
-      const customProviders = JSON.parse(customProvidersJSONString);
-      return customProviders;
+    if (JSONString) {
+      return JSON.parse(JSONString);
     }
   } catch (error) {
-    vscode.window.showErrorMessage('Error parsing terraform-documentation-quick-links.providers setting. Please ensure it is valid JSON.');
-    console.error('Error parsing customProvidersJSON:', error);
+    vscode.window.showErrorMessage(`Error parsing terraform-documentation-quick-links.${name} setting. Please ensure it is valid JSON.`);
+    console.error('Error parsing terraform-documentation-quick-links.${name}:', error);
     return {};
   }
 }
 
-export function generateDocumentationUrl(baseurl: string, resourceType: string, resource: string, customProviders: Record<string, string>) {
+// Actual logic
+
+export function generateDocumentationUrl(
+  baseurl: string,
+  type: string,
+  resource: string,
+  userOverrides: Record<string, Record<string, string>>,
+) {
   const provider = resource.substring(0, resource.indexOf('_'));
-  const providers = providerData as Record<string, string>;
 
-  const providerPath = customProviders[provider] as string || providers[provider] as string || `hashicorp/${provider}`;
+  const providerPath = userOverrides.provider[provider] as string || overrides.provider[provider] as string || `hashicorp/${provider}`;
+  const resourcePath = userOverrides[type][resource] as string || overrides[type][resource] as string || resource.substring(provider.length + 1, resource.length);
 
-  return `${baseurl}/providers/${providerPath}/latest/docs/${resourceTypes[resourceType]}/${resource.substring(provider.length + 1, resource.length)}`;
+  return `${baseurl}/providers/${providerPath}/latest/docs/${types[type]}/${resourcePath}`;
 }
 
 export class TerraformLinker implements vscode.DocumentLinkProvider {
   provideDocumentLinks(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.ProviderResult<vscode.DocumentLink[]> {
     const documentLinks: vscode.DocumentLink[] = [];
-    const customProviders = getCustomProviders();
-    const baseurl = configuration.get('baseurl') as string;
 
     const regEx = /(resource|data)\s+"([^"]+)"\s+"([^"]+)"/g;
     let match;
 
     while ((match = regEx.exec(document.getText()))) {
       console.log(match);
-      const resourceType = match[1];
+      const type = match[1];
       const resource = match[2];
 
       const startIndex = match.index + match[0].indexOf(resource);
       const endIndex = startIndex + resource.length;
       const range = new vscode.Range(document.positionAt(startIndex), document.positionAt(endIndex));
 
-      const uri = vscode.Uri.parse(generateDocumentationUrl(baseurl, resourceType, resource, customProviders));
+      const uri = vscode.Uri.parse(
+        generateDocumentationUrl(baseurl, type, resource, userOverrides)
+      );
       const link = new vscode.DocumentLink(range, uri);
       documentLinks.push(link);
     }
--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -2,19 +2,25 @@ import * as assert from 'assert';
 import * as vscode from 'vscode';
 import { generateDocumentationUrl } from '../link-generator';
 
+const emptyUserOverrides = {
+	provider: {},
+	resource: {},
+	data: {}
+};
+
 suite('Generate Terraform Documentation Url Unit Tests', () => {
 	suite('Official providers', () => {
 		const testCases = {
-			"aws_instance": "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance",
-			"google_compute_instance": "https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance",
-			"azurerm_virtual_machine": "https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_machine",
-			"kubernetes_pod": "https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs/resources/pod",
+			aws_instance: "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance",
+			google_compute_instance: "https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance",
+			azurerm_virtual_machine: "https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_machine",
+			kubernetes_pod: "https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs/resources/pod",
 		};
 
 		for (const [key, value] of Object.entries(testCases)) {
 			test(key, () => {
 				assert.strictEqual(
-					generateDocumentationUrl("https://registry.terraform.io", "resource", key, {}), value
+					generateDocumentationUrl("https://registry.terraform.io", "resource", key, emptyUserOverrides), value
 				);
 			});
 		};
@@ -29,7 +35,7 @@ suite('Generate Terraform Documentation Url Unit Tests', () => {
 		for (const [key, value] of Object.entries(testCases)) {
 			test(key, () => {
 				assert.strictEqual(
-					generateDocumentationUrl("https://registry.terraform.io", "resource", key, {}), value
+					generateDocumentationUrl("https://registry.terraform.io", "resource", key, emptyUserOverrides), value
 				);
 			});
 		};
@@ -38,48 +44,102 @@ suite('Generate Terraform Documentation Url Unit Tests', () => {
 	suite('Unofficial providers not included in default provider or included in override setting fallback to assuming official provider', () => {
 		test("aquasecurity", () => {
 			assert.strictEqual(
-				generateDocumentationUrl("https://registry.terraform.io", "resource", "aquasec_image", {}), "https://registry.terraform.io/providers/hashicorp/aquasec/latest/docs/resources/image"
+				generateDocumentationUrl("https://registry.terraform.io", "resource", "aquasec_image", emptyUserOverrides), "https://registry.terraform.io/providers/hashicorp/aquasec/latest/docs/resources/image"
 			);
 		});
 	});
 
-	suite('Unofficial providers not included in default provider but included in override setting', () => {
+	suite('Unofficial providers not included in default providers but included in user override data', () => {
 		test("aquasecurity", () => {
 			assert.strictEqual(
-				generateDocumentationUrl("https://registry.terraform.io", "resource", "aquasec_image", { "aquasec": "aquasecurity/aquasec" }), "https://registry.terraform.io/providers/aquasecurity/aquasec/latest/docs/resources/image"
+				generateDocumentationUrl(
+					"https://registry.terraform.io",
+					"resource",
+					"aquasec_image",
+					{ provider: { aquasec: "aquasecurity/aquasec" }, resource: {}, data: {} }
+				),
+				"https://registry.terraform.io/providers/aquasecurity/aquasec/latest/docs/resources/image"
 			);
 		});
 	});
 
-	suite('Unofficial providers included in default provider and included in override setting give preference to user setting', () => {
+	suite('Unofficial providers included in default provider and included in user override setting give preference to user setting', () => {
 		test("aquasecurity", () => {
 			assert.strictEqual(
-				generateDocumentationUrl("https://registry.terraform.io", "resource", "gitlab_project", { "gitlab": "gitlabhq/gitlab-beta" }), "https://registry.terraform.io/providers/gitlabhq/gitlab-beta/latest/docs/resources/project"
+				generateDocumentationUrl(
+					"https://registry.terraform.io",
+					"resource",
+					"gitlab_project",
+					{ provider: { gitlab: "gitlabhq/gitlab-beta" }, resource: {}, data: {} }
+				),
+				"https://registry.terraform.io/providers/gitlabhq/gitlab-beta/latest/docs/resources/project"
 			);
 		});
 	});
 
-	suite('LInking to Opentofu docs by changing baseurl setting', () => {
+	suite('Linking to Opentofu docs by changing baseurl setting', () => {
 		test("aquasecurity", async () => {
 			assert.strictEqual(
-				generateDocumentationUrl("https://library.tf", "resource", "gitlab_project", { "gitlab": "gitlabhq/gitlab-beta" }),
-				"https://library.tf/providers/gitlabhq/gitlab-beta/latest/docs/resources/project"
+				generateDocumentationUrl("https://library.tf", "data", "cloudinit_config", emptyUserOverrides),
+				"https://library.tf/providers/hashicorp/cloudinit/latest/docs/data-sources/config"
 			);
 		});
 	});
 
 	suite('Official providers / Data Sources', () => {
 		const testCases = {
-			"aws_instance": "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/instance",
-			"google_compute_instance": "https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/compute_instance"
+			aws_instance: "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/instance",
+			google_compute_instance: "https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/compute_instance"
 		};
 
 		for (const [key, value] of Object.entries(testCases)) {
 			test(key, () => {
 				assert.strictEqual(
-					generateDocumentationUrl("https://registry.terraform.io", "data", key, {}), value
+					generateDocumentationUrl("https://registry.terraform.io", "data", key, emptyUserOverrides), value
 				);
 			});
 		};
+	});
+
+	suite('Resources in override setting', () => {
+		test("google_service_account", () => {
+			assert.strictEqual(
+				generateDocumentationUrl(
+					"https://registry.terraform.io",
+					"resource",
+					"google_service_account",
+					emptyUserOverrides,
+				),
+				"https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_service_account"
+			);
+		});
+	});
+
+	suite('DataSource in override setting', () => {
+		test("google_service_account", () => {
+			assert.strictEqual(
+				generateDocumentationUrl(
+					"https://registry.terraform.io",
+					"data",
+					"google_project_service",
+					emptyUserOverrides,
+				),
+				"https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/google_project_service"
+			);
+		});
+	});
+
+	suite('DataSource in user override setting', () => {
+		test("google_service_account", () => {
+			assert.strictEqual(
+				generateDocumentationUrl(
+					"https://registry.terraform.io",
+					"data",
+					"google_project_service",
+					{ provider: {}, resource: {}, data: { google_project_service: "google_google_project_service" } },
+				),
+				"https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/google_google_project_service"
+			);
+		});
 	});
 });
